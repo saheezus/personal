b8f564c914bbb973799b5e8ebbd80f0c5ab026b1
Sahi C <sahi.chitrapu@gmail.com>
2025-03-02 01:46:11 -0800

    added scroll transition

--- personal/src/App.js
+++ personal/src/App.js
@@ -31,13 +31,20 @@ const App = () => {
     };
   }, []);
 
+  // Change the background color based on the scroll position
+  const bgColor = `rgb(${(Math.min(yPosition, 2 * window.innerHeight) / window.innerHeight) * 255}, 
+                        ${(Math.min(yPosition, 2 * window.innerHeight) / window.innerHeight) * 255}, 
+                        ${(Math.min(yPosition, 2 * window.innerHeight) / window.innerHeight) * 255})`;
+
   const handleScroll = () => {
     setYPosition(window.scrollY);
   }
 
+  // Keep track of the scroll position
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     console.log(yPosition);
+    console.log(bgColor);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -45,34 +52,37 @@ const App = () => {
   }, [yPosition]);
 
   return (
-    <div className="bg-black">
+    <div className="text-white" style={{backgroundColor: bgColor}}>
 
-      <div className="fixed bg-black text-white h-screen p-7 justify-center items-left flex flex-col invisible md:visible">
+      <div 
+        className="fixed text-white h-screen p-7 justify-center items-left flex flex-col invisible md:visible" 
+        style = {{backgroundColor: bgColor, color: yPosition <= window.innerHeight ? 'white' : 'black'}}
+      >
         <div className="p-1">
-          <span className="text-l font-bold hover:text-gray-400 transition-colors duration-300 mx-3">Me</span>
+          <span className="text-l font-bold hover:opacity-50 transition-opacity duration-300 mx-3">Me</span>
         </div>
         <div className="p-1">
-          <span className="text-l font-bold hover:text-gray-400 transition-colors duration-300 mx-3">Projects</span>
+          <span className="text-l font-bold hover:opacity-50 transition-opacity duration-300 mx-3">Projects</span>
         </div>
         <div className="p-1">
-          <span className="text-l font-bold hover:text-gray-400 transition-colors duration-300 mx-3">Experience</span>
+          <span className="text-l font-bold hover:opacity-50 transition-opacity duration-300 mx-3">Experience</span>
         </div>
         <div className="p-4">
           <img 
-            src={require("./img/github-logo.png")}
+            src={yPosition <= window.innerHeight ? require("./img/github-logo.png") : require("./img/github-logo2.png")}
             className="h-7 w-7 hover:opacity-50 transition-opacity duration-300"
           />
         </div>
         <div className="p-4">
           <img 
-            src={require("./img/linkedin-logo.png")}
+            src={yPosition <= window.innerHeight ? require("./img/linkedin-logo.png") : require("./img/linkedin-logo2.png")}
             className="h-7 w-8 hover:opacity-50 transition-opacity duration-300"
           />
         </div>
       </div>
 
       {/* Main Content */}
-      <div className="bg-black text-white justify-center items-center flex flex-col h-screen">
+      <div className="justify-center items-center flex flex-col h-screen" style={{backgroundColor: bgColor}}>
         <div className="mx-auto justify-center p-4">
           <img 
             src={require("./img/me.jpeg")} 
@@ -86,7 +96,10 @@ const App = () => {
           <span className="" ref={typedDescriptions}></span>
         </div>
       </div>
-      <div className="bg-black text-white h-screen">
+      <div className="text-white h-screen" style={{backgroundColor: bgColor}}>
+
+      </div>
+      <div className="bg-white text-black h-screen">
 
       </div>
     </div>
